9b0d2e409bc683d2c21c1e6ae71ffe4b8e51bac5
Nazrin Muradli <147836567+Httpsnazrin@users.noreply.github.com>
2025-02-08 23:19:00 +0400

    Update Human.java

--- HappyFamily01/Human.java
+++ HappyFamily01/Human.java
@@ -1,65 +1,65 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class Human {
-     String name;
+    String name;
     String surname;
-      int yearOfBirth;
-      int iq;
-      Pet pet;
-      Human mother;
-      Human father;
-      String[][] schedule;
+    int year;
+    int iq;
+    Pet pet;
+    Human mother;
+    Human father;
+    String[][] schedule;
 
-    public Human(String name, String surname, int yearOfBirth, int iq, Pet pet, Human mother, Human father, String[][] schedule) {
-        this.name = name;
-        this.surname = surname;
-        this.yearOfBirth = yearOfBirth;
-        this.iq = iq;
-        this.pet = pet;
-        this.mother = mother;
-        this.father = father;
-        this.schedule = schedule;
+    public Human() {
+
     }
 
-    public Human(String vito, String karleone, int yearOfBirth) {
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+    public Human(String name, String surname, int year, Human father, Human mother) {
+        this(name, surname, year);
+        this.father = father;
+        this.mother = mother;
+    }
+
+    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father) {
+        this(name, surname, year, father, mother);
+        this.iq = iq;
+        this.pet = pet;
     }
 
     public void greetPet() {
         if (pet != null) {
             System.out.println("Hello, " + pet.nickname);
-        } else {
-            System.out.println("I don't have a pet.");
         }
     }
 
     public void describePet() {
         if (pet != null) {
-            String slyness = pet.trickLevel > 50 ? "very sly" : "almost not sly";
-            System.out.println("I have a " + pet.species + " named " + pet.nickname + " who is " + pet.age + " years old, and he is " + slyness + ".");
-        } else {
-            System.out.println("I don't have a pet.");
+            if (pet.trickLevel > 50) {
+                System.out.println("I have a " + pet.species + " that is " + pet.age + " years old, he is very sly.");
+            } else {
+                System.out.println("I have a " + pet.species + " that is " + pet.age + " years old, he is almost not sly.");
+            }
         }
     }
 
     public boolean feedPet(boolean isItTimeForFeeding) {
-        if (pet == null) {
-            System.out.println("I don't have a pet to feed.");
-            return false;
-        }
-
         if (isItTimeForFeeding) {
-            System.out.println("Hm... I will feed " + name + "'s " + pet.species + ".");
+            System.out.println("Hm... I will feed " + name + "'s " + pet.species);
             return true;
         } else {
             Random random = new Random();
-            int randomNumber = random.nextInt(101); // Generates a number from 0 to 100
-
+            int randomNumber = random.nextInt(101);
             if (pet.trickLevel > randomNumber) {
-                System.out.println("Hm... I will feed " + name + "'s " + pet.species + ".");
+                System.out.println("Hm... I will feed " + name + "'s " + pet.species);
                 return true;
             } else {
-                System.out.println("I think " + name + " is not hungry.");
+                System.out.println("I think " + name + "'s " + pet.species + " is not hungry.");
                 return false;
             }
         }
@@ -67,10 +67,8 @@ public class Human {
 
     @Override
     public String toString() {
-        return "Human{name='" + name + "', surname='" + surname + "', year=" + yearOfBirth +
-                ", iq=" + iq +
-                ", mother=" + (mother != null ? mother.name + " " + mother.surname : "unknown") +
-                ", father=" + (father != null ? father.name + " " + father.surname : "unknown") +
-                ", pet=" + (pet != null ? pet.toString() : "No pet") + "}";
+        return "Human{name='" + name + "', surname='" + surname + "', year=" + year + ", iq=" + iq +
+                ", mother=" + mother.name + " " + mother.surname + ", father=" + father.name + " " + father.surname +
+                ", pet=" + pet.toString() + "}";
     }
 }
