cbb49684ed8dd9f231ab30f4f37a0761b45f89c1
Nazrin Muradli <147836567+Httpsnazrin@users.noreply.github.com>
2025-02-08 23:18:21 +0400

    Update Main.java

--- HappyFamily01/Main.java
+++ HappyFamily01/Main.java
@@ -1,22 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Creating the Pet object
-        String[] petHabits = {"eat", "drink", "sleep"};
-        Pet pet = new Pet("dog", "Rock", 5, 75, petHabits);
-
-        // Creating the Human objects (father, mother, and child)
+        Pet pet = new Pet("cat", "Mestan", 2, 54, new String[]{"meow", "eat", "sleep"});
         Human mother = new Human("Nazrin", "Muradli", 2005);
-        Human father = new Human("Viktor", "Samedov", 2000);
-        String[][] schedule = {{"Monday", "Gym"}, {"Tuesday", "Reading"}, {"Wednesday", "Cooking"}};
-        Human child = new Human("Michael", "Karleone", 2000, 90, pet, mother, father, schedule);
+        Human father = new Human("Murad", "Guliyev", 2006);
+        Human human = new Human("Asmar", "Karimli", 2006, 150, pet, mother , father);
 
-        // Testing feedPet method
-        System.out.println("\nFeeding Pet when it's time:");
-        boolean fed = child.feedPet(true);
-        System.out.println("Feeding happened: " + fed);
-
-        System.out.println("\nFeeding Pet when it's NOT time:");
-        fed = child.feedPet(false);
-        System.out.println("Feeding happened: " + fed);
+        human.greetPet();
+        human.describePet();
+        pet.eat();
+        pet.foul();
+        pet.respond();
     }
 }
